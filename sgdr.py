9090b0795a8db570308d189a6d2e474d0e8c7fe1
Peter Rong <PeterRong96@gmail.com>
2018-01-02 20:55:31 +0800

    sgdr
    Signed-off-by: Peter Rong <PeterRong96@gmail.com>

--- sgdr.py
+++ sgdr.py
@@ -10,17 +10,17 @@ class CosineLR(_LRScheduler):
         self.step_size_min = step_size_min
         self.t0 = t0
         self.tmult = tmult
-        self.epochs_since_restart = curr_epoch
+        self.epochs = curr_epoch
         super(CosineLR, self).__init__(optimizer, last_epoch)
 
     def get_lr(self):
-        self.epochs_since_restart += 1
+        self.epochs += 1
 
-        if self.epochs_since_restart > self.t0:
+        if self.epochs > self.t0:
             self.t0 *= self.tmult
-            self.epochs_since_restart = 0
+            self.epochs = 0
 
-        lrs = [self.step_size_min + (0.5 * (base_lr - self.step_size_min) * (1 + cos(self.epochs_since_restart * pi / self.t0)))
+        lrs = [self.step_size_min + (0.5 * (base_lr - self.step_size_min) * (1 + cos(self.epochs * pi / self.t0)))
                 for base_lr in self.base_lrs]
 
         return lrs
