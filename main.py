57584a9eeaf8a1f4f4c913bf1aba9d97fe836d0a
Peter Rong <PeterRong96@gmail.com>
2018-01-02 21:49:19 +0800

    files added
    Signed-off-by: Peter Rong <PeterRong96@gmail.com>

---
+++ main.py
@@ -0,0 +1,326 @@
+import torch
+import torchvision
+import torchsample.transforms
+import numpy as np
+import time
+import os
+import matplotlib
+from tqdm import tqdm
+
+# Add this to save all the plot
+matplotlib.use('Agg')
+import matplotlib.pyplot as plt
+from util import get_x_views, TransformDataset
+from skimage import io
+from mvcnn import mvcnn
+from sgdr import CosineLR
+
+TEST_MODEL = False          # Creates predictions file
+DEBUG = False               # Loads small dataset and plots augmented images for debugging
+VIEW_COUNT_TOTAL = 16       # Total number of views in our scans. APS files have 16.
+VIEW_COUNT_SAMPLE = 16      # Total number of views sampled from the scan. I now use all 16 and this line isn't needed.
+epochs = 25
+state_dict = '/home/dingpeng/src/predictions/14825333/model_50.torch'           # Loads a previous state of the model for picking back up training or making predictions.
+opt_dict = '/home/dingpeng/src/predictions/14825333/opt_50.torch'             # Loads a previous state of the optimizer for picking back up training if it was cut short.
+
+class AverageMeter(object):
+    """Computes and stores the average and current value"""
+    ''' Learned from '''
+    def __init__(self):
+        self.reset()
+
+    def reset(self):
+        self.val = 0
+        self.avg = 0
+        self.sum = 0
+        self.count = 0
+
+    def update(self, val, n=1):
+        self.val = val
+        self.sum += val * n
+        self.count += n
+        self.avg = self.sum / self.count
+
+def train(train_loader, model, criterion, optimizer, scheduler, epoch):
+    batch_time = AverageMeter()
+    data_time = AverageMeter()
+    losses = AverageMeter()
+
+    # switch to train mode
+    model.train()
+    
+    end = time.time()
+    for i, (batch, target) in enumerate(train_loader):
+        # measure data loading time
+        data_time.update(time.time() - end)
+
+        batch = batch.cuda(async=True)
+        target = target.cuda(async=True)
+
+        batch = batch.clone().repeat(1, 1, 3, 1, 1)
+
+        if DEBUG and False:
+            print(list(zip(target[0].cpu().tolist(), [x+1 for x in range(17)])))
+            print(list(zip(target[1].cpu().tolist(), [x+1 for x in range(17)])))
+            for j in range(VIEW_COUNT_SAMPLE):
+                plt.imshow(batch.cpu().numpy()[0, j, 0])
+                plt.show()
+
+        batch = torch.autograd.Variable(batch)
+        target = torch.autograd.Variable(target)
+
+        # compute output
+        output = model(batch)
+        loss = criterion(output, target)
+
+        # measure accuracy and record loss
+        losses.update(loss.data[0], batch.size(0))
+
+        # compute gradient and do SGD step
+        loss.backward()
+        optimizer.step()
+        optimizer.zero_grad()
+
+        # measure elapsed time
+        batch_time.update(time.time() - end)
+        end = time.time()
+
+        if i % 10 == 0:
+            print('Epoch: [{0}/{1}][{2}/{3}]\t'
+                  'Time {batch_time.val:.3f} ({batch_time.avg:.3f})\t'
+                  'Data {data_time.val:.3f} ({data_time.avg:.3f})\t'
+                  'Loss {loss.val:.4f} ({loss.avg:.4f})\t'.format(
+                   epoch, epochs, i, len(train_loader), batch_time=batch_time,
+                   data_time=data_time, loss=losses))
+
+        del batch, target, output, loss
+
+    scheduler.step()
+    loss_tracker_train.append(losses.avg)
+
+def validate(val_loader, model, criterion):
+    batch_time = AverageMeter()
+    data_time = AverageMeter()
+    losses = AverageMeter()
+
+    model.eval()
+
+    end = time.time()
+    for i, (input, target) in enumerate(val_loader):
+        # measure data loading time
+        data_time.update(time.time() - end)
+
+        input = input.cuda(async=True)
+        target = target.cuda(async=True)
+
+        input = input.clone().repeat(1, 1, 3, 1, 1)
+
+        if DEBUG and False:
+            print(list(zip(target[0].cpu().tolist(), [x+1 for x in range(17)])))
+            print(list(zip(target[1].cpu().tolist(), [x+1 for x in range(17)])))
+            for j in range(VIEW_COUNT_SAMPLE):
+                plt.imshow(input.cpu().numpy()[0, j, 0])
+                plt.show()
+
+        input = torch.autograd.Variable(input)
+        target = torch.autograd.Variable(target)
+
+        # compute output
+        output = model(input)
+        loss = criterion(output, target)
+
+        # measure accuracy and record loss
+        losses.update(loss.data[0], input.size(0))
+
+        # measure elapsed time
+        batch_time.update(time.time() - end)
+        end = time.time()
+
+        print('VALIDATION:')
+        print('Time {batch_time.val:.3f} ({batch_time.avg:.3f})\t'
+                'Data {data_time.val:.3f} ({data_time.avg:.3f})\t'
+                'Loss {loss.val:.4f} ({loss.avg:.4f})\t'.format(batch_time=batch_time,
+                data_time=data_time, loss=losses))
+        
+        del input, target, output, loss
+
+    loss_tracker_val.append(losses.avg)
+
+def predict(model, name):
+    new_test = name_to_array(name, "test")
+
+    # Imitate a batch
+    new_test = np.expand_dims(new_test, 0)
+    new_test = torch.Tensor(new_test)
+    if DEBUG and False:
+        for j in range(VIEW_COUNT_SAMPLE):
+            plt.imshow(new_test.numpy()[0, j, 0, :, :])
+            plt.show()
+    # new_test = new_test.repeat(1,1,3,1,1)
+    new_test = new_test.clone().repeat(1,1,3,1,1)
+    input_var = torch.autograd.Variable(new_test.cuda(), requires_grad=False)
+
+    accur = 0
+
+    # Run through network
+    if type(model) != list:
+        model = [model]
+    for m in model:
+        output = torch.nn.Sigmoid()(m(input_var))
+        output = output.data.cpu().numpy()[0]
+        accur += output
+
+    return accur / len(model)
+
+def test_model(model, base_dir=None, epoch=0):
+    time_str = str(int(time.time()))[2:]
+    if base_dir == None:
+        base_dir = "predictions/{}".format(time_str)
+        os.mkdir(base_dir)
+    outfile = open('{}/predictions_{}_{}.csv'.format(base_dir, time_str, epoch), 'w+')
+    print('Id,Probability', file=outfile)
+    test_names = set([filename.split('.')[0] for filename in os.listdir('test/')])
+    for name in test_names:
+        print(name)
+        for bodypart, prob in enumerate(predict(model, name)):
+            print("{}_Zone{},{}".format(name, bodypart + 1, prob), file=outfile)
+
+def name_to_array(name, path):
+    # Given a name and path, return an array of images.
+    array = np.array(get_x_views("{}/{}.{}".format(path, name, "aps"), x=VIEW_COUNT_TOTAL))
+    array = np.expand_dims(array, 1)
+    array = np.pad(array, ((0,0), (0,0), (0, 1), (0, 0)), mode="constant", constant_values=0)
+    
+    return array
+
+
+if TEST_MODEL:
+    print("Testing model only")
+    models = []
+    if type(state_dict) != list:
+        state_dict = [state_dict]
+    for name in state_dict:
+        model = mvcnn(17, pretrained=True).cuda()
+        model.load_state_dict(torch.load(name))
+        model.eval()
+        models.append(model)
+        print("Added {}".format(name))
+    test_model(models)
+    exit()
+
+print("Initializing model")
+model = mvcnn(17, pretrained=True).cuda()
+
+# Create dictionary matching name to vector
+train_file = open('stage1_labels.csv')
+train_file.readline()
+# Use dict for easier indexing.
+name_to_vector = {}
+for line in train_file:
+    name_zone, label = line.strip().split(',')
+    name, zone = name_zone.split('_')
+    # Take the number out.
+    zone = int(zone[4:])
+    if name not in name_to_vector:
+        name_to_vector[name] = np.zeros(17)
+    name_to_vector[name][zone-1] += int(label)
+
+# If debug, only take 16 images for training.
+if DEBUG:
+    name_to_vector = {k: name_to_vector[k] for k in sorted(name_to_vector.keys())[:16]}
+name_to_vector = list(name_to_vector.items())
+print("Loading Images...")
+
+# Convert to raw training input and output
+# Images are (660, 512)
+sample_cnt = len(name_to_vector)
+print("{} samples found.".format(sample_cnt))
+
+names = [None] * sample_cnt
+training_input = np.empty((sample_cnt, VIEW_COUNT_TOTAL, 1, 660  + 1, 512  + 0), dtype=np.float32)
+training_output = np.empty((sample_cnt, 17))
+for i in tqdm(range(sample_cnt)):
+    name, is_danger = name_to_vector[i]
+    input_tensor = name_to_array(name, "aps")
+    training_input[i] = input_tensor
+    training_output[i] = is_danger
+    names[i] = name
+print("Images Loading Finished.")
+
+
+print("Splitting into train/validation sets...", end='')
+# Too few valid! add more later.
+training_split = len(training_input) - 5
+training_input, valid_input = training_input[0:training_split], training_input[training_split:]
+training_output, valid_output = training_output[0:training_split], training_output[training_split:]
+print("Split Finished.")
+print("There are {} training images with size {}".format(training_split, training_input.shape))
+print("There are {} training images with size {}".format(5, valid_input.shape))
+
+print("Creating DataLoaders...")
+training_input = torch.Tensor(training_input)
+training_output = torch.Tensor(training_output)
+valid_input = torch.Tensor(valid_input)
+valid_output = torch.Tensor(valid_output)
+
+dataset = TransformDataset(training_input, training_output, names, train=True)
+data_loader = torch.utils.data.DataLoader(dataset, batch_size=2, shuffle=True, sampler=None, batch_sampler=None, num_workers=8, pin_memory=True, drop_last=True)
+valid_dataset = TransformDataset(valid_input, valid_output, names, train=False)
+valid_data_loader = torch.utils.data.DataLoader(valid_dataset, batch_size=2, shuffle=False, sampler=None, batch_sampler=None, num_workers=0, pin_memory=True, drop_last=False)
+
+criterion = torch.nn.BCEWithLogitsLoss().cuda()
+optimizer = torch.optim.SGD(model.parameters(), lr=1e-2, momentum=0.9, dampening=0, weight_decay=1e-4, nesterov=True)
+scheduler = CosineLR(optimizer, step_size_min=1e-4, t0=200, tmult=1)
+# Try this for benchmark later.
+# scheduler = StepLR(optimizer, step_size = 0.5)
+if state_dict:
+    model.load_state_dict(torch.load(state_dict))
+    optimizer.load_state_dict(torch.load(opt_dict))
+    print("Loaded old weights")
+
+torch.backends.cudnn.benchmark = False
+
+print("Beginning training...")
+time_str = str(int(time.time()))[2::]
+# Path to save models and predictions
+base_dir = "predictions/{}".format(time_str)
+if not os.path.exists(base_dir):
+    os.mkdir(base_dir)
+
+loss_tracker_train = []
+loss_tracker_val = []
+best_loss = 0.001 # Ideal.
+this_loss = 0xffff
+
+for epoch in range(epochs+1):
+    train(data_loader, model, criterion, optimizer, scheduler, epoch)
+    validate(valid_data_loader, model, criterion)
+
+    if epoch and epoch % 25 == 0 or DEBUG:
+
+        print("Saving Model ...", end = "")
+        if not DEBUG:
+            torch.save(model.state_dict(), "{}/model_{}.torch".format(base_dir, epoch))
+            torch.save(optimizer.state_dict(), "{}/opt_{}.torch".format(base_dir, epoch))
+        print("Model Saved.")
+        print("Plotting train/valid loss ...", end = "")
+        # Save a plot of the average loss over time
+        plt.clf()
+        plt.plot(loss_tracker_train[1:], label="Training loss")
+        plt.plot(loss_tracker_val[1:], label="Validation loss")
+        plt.legend(loc="upper left")
+        if not DEBUG:
+            plt.savefig("{}/predictions_{}.png".format(base_dir, epoch))
+        print("Plot Finished.")
+
+        print("Predicting...")
+        test_model(model, base_dir, epoch)
+
+    this_loss = loss_tracker_val[-1]
+    print("This loss: {}".format(this_loss))
+    print("Best loss: {}".format(best_loss))
+
+    if this_loss < best_loss + 0.0025:
+        print("Found better model with {} loss (old loss was {})".format(this_loss, best_loss))
+        best_loss = min(this_loss, best_loss)
+        torch.save(model.state_dict(), "{}/best_model_{}_{:.4f}.torch".format(base_dir, epoch, this_loss))
---
+++ util.py
@@ -0,0 +1,191 @@
+import numpy as np
+import os
+import shutil
+import psutil
+import torch
+import torchsample
+import torchvision
+
+def read_header(infile):
+    """Read image header (first 512 bytes)
+    """
+    h = dict()
+    fid = open(infile, 'r+b')
+    h['filename'] = b''.join(np.fromfile(fid, dtype = 'S1', count = 20))
+    h['parent_filename'] = b''.join(np.fromfile(fid, dtype = 'S1', count = 20))
+    h['comments1'] = b''.join(np.fromfile(fid, dtype = 'S1', count = 80))
+    h['comments2'] = b''.join(np.fromfile(fid, dtype = 'S1', count = 80))
+    h['energy_type'] = np.fromfile(fid, dtype = np.int16, count = 1)
+    h['config_type'] = np.fromfile(fid, dtype = np.int16, count = 1)
+    h['file_type'] = np.fromfile(fid, dtype = np.int16, count = 1)
+    h['trans_type'] = np.fromfile(fid, dtype = np.int16, count = 1)
+    h['scan_type'] = np.fromfile(fid, dtype = np.int16, count = 1)
+    h['data_type'] = np.fromfile(fid, dtype = np.int16, count = 1)
+    h['date_modified'] = b''.join(np.fromfile(fid, dtype = 'S1', count = 16))
+    h['frequency'] = np.fromfile(fid, dtype = np.float32, count = 1)
+    h['mat_velocity'] = np.fromfile(fid, dtype = np.float32, count = 1)
+    h['num_pts'] = np.fromfile(fid, dtype = np.int32, count = 1)
+    h['num_polarization_channels'] = np.fromfile(fid, dtype = np.int16, count = 1)
+    h['spare00'] = np.fromfile(fid, dtype = np.int16, count = 1)
+    h['adc_min_voltage'] = np.fromfile(fid, dtype = np.float32, count = 1)
+    h['adc_max_voltage'] = np.fromfile(fid, dtype = np.float32, count = 1)
+    h['band_width'] = np.fromfile(fid, dtype = np.float32, count = 1)
+    h['spare01'] = np.fromfile(fid, dtype = np.int16, count = 5)
+    h['polarization_type'] = np.fromfile(fid, dtype = np.int16, count = 4)
+    h['record_header_size'] = np.fromfile(fid, dtype = np.int16, count = 1)
+    h['word_type'] = np.fromfile(fid, dtype = np.int16, count = 1)
+    h['word_precision'] = np.fromfile(fid, dtype = np.int16, count = 1)
+    h['min_data_value'] = np.fromfile(fid, dtype = np.float32, count = 1)
+    h['max_data_value'] = np.fromfile(fid, dtype = np.float32, count = 1)
+    h['avg_data_value'] = np.fromfile(fid, dtype = np.float32, count = 1)
+    h['data_scale_factor'] = np.fromfile(fid, dtype = np.float32, count = 1)
+    h['data_units'] = np.fromfile(fid, dtype = np.int16, count = 1)
+    h['surf_removal'] = np.fromfile(fid, dtype = np.uint16, count = 1)
+    h['edge_weighting'] = np.fromfile(fid, dtype = np.uint16, count = 1)
+    h['x_units'] = np.fromfile(fid, dtype = np.uint16, count = 1)
+    h['y_units'] = np.fromfile(fid, dtype = np.uint16, count = 1)
+    h['z_units'] = np.fromfile(fid, dtype = np.uint16, count = 1)
+    h['t_units'] = np.fromfile(fid, dtype = np.uint16, count = 1)
+    h['spare02'] = np.fromfile(fid, dtype = np.int16, count = 1)
+    h['x_return_speed'] = np.fromfile(fid, dtype = np.float32, count = 1)
+    h['y_return_speed'] = np.fromfile(fid, dtype = np.float32, count = 1)
+    h['z_return_speed'] = np.fromfile(fid, dtype = np.float32, count = 1)
+    h['scan_orientation'] = np.fromfile(fid, dtype = np.int16, count = 1)
+    h['scan_direction'] = np.fromfile(fid, dtype = np.int16, count = 1)
+    h['data_storage_order'] = np.fromfile(fid, dtype = np.int16, count = 1)
+    h['scanner_type'] = np.fromfile(fid, dtype = np.int16, count = 1)
+    h['x_inc'] = np.fromfile(fid, dtype = np.float32, count = 1)
+    h['y_inc'] = np.fromfile(fid, dtype = np.float32, count = 1)
+    h['z_inc'] = np.fromfile(fid, dtype = np.float32, count = 1)
+    h['t_inc'] = np.fromfile(fid, dtype = np.float32, count = 1)
+    h['num_x_pts'] = np.fromfile(fid, dtype = np.int32, count = 1)
+    h['num_y_pts'] = np.fromfile(fid, dtype = np.int32, count = 1)
+    h['num_z_pts'] = np.fromfile(fid, dtype = np.int32, count = 1)
+    h['num_t_pts'] = np.fromfile(fid, dtype = np.int32, count = 1)
+    h['x_speed'] = np.fromfile(fid, dtype = np.float32, count = 1)
+    h['y_speed'] = np.fromfile(fid, dtype = np.float32, count = 1)
+    h['z_speed'] = np.fromfile(fid, dtype = np.float32, count = 1)
+    h['x_acc'] = np.fromfile(fid, dtype = np.float32, count = 1)
+    h['y_acc'] = np.fromfile(fid, dtype = np.float32, count = 1)
+    h['z_acc'] = np.fromfile(fid, dtype = np.float32, count = 1)
+    h['x_motor_res'] = np.fromfile(fid, dtype = np.float32, count = 1)
+    h['y_motor_res'] = np.fromfile(fid, dtype = np.float32, count = 1)
+    h['z_motor_res'] = np.fromfile(fid, dtype = np.float32, count = 1)
+    h['x_encoder_res'] = np.fromfile(fid, dtype = np.float32, count = 1)
+    h['y_encoder_res'] = np.fromfile(fid, dtype = np.float32, count = 1)
+    h['z_encoder_res'] = np.fromfile(fid, dtype = np.float32, count = 1)
+    h['date_processed'] = b''.join(np.fromfile(fid, dtype = 'S1', count = 8))
+    h['time_processed'] = b''.join(np.fromfile(fid, dtype = 'S1', count = 8))
+    h['depth_recon'] = np.fromfile(fid, dtype = np.float32, count = 1)
+    h['x_max_travel'] = np.fromfile(fid, dtype = np.float32, count = 1)
+    h['y_max_travel'] = np.fromfile(fid, dtype = np.float32, count = 1)
+    h['elevation_offset_angle'] = np.fromfile(fid, dtype = np.float32, count = 1)
+    h['roll_offset_angle'] = np.fromfile(fid, dtype = np.float32, count = 1)
+    h['z_max_travel'] = np.fromfile(fid, dtype = np.float32, count = 1)
+    h['azimuth_offset_angle'] = np.fromfile(fid, dtype = np.float32, count = 1)
+    h['adc_type'] = np.fromfile(fid, dtype = np.int16, count = 1)
+    h['spare06'] = np.fromfile(fid, dtype = np.int16, count = 1)
+    h['scanner_radius'] = np.fromfile(fid, dtype = np.float32, count = 1)
+    h['x_offset'] = np.fromfile(fid, dtype = np.float32, count = 1)
+    h['y_offset'] = np.fromfile(fid, dtype = np.float32, count = 1)
+    h['z_offset'] = np.fromfile(fid, dtype = np.float32, count = 1)
+    h['t_delay'] = np.fromfile(fid, dtype = np.float32, count = 1)
+    h['range_gate_start'] = np.fromfile(fid, dtype = np.float32, count = 1)
+    h['range_gate_end'] = np.fromfile(fid, dtype = np.float32, count = 1)
+    h['ahis_software_version'] = np.fromfile(fid, dtype = np.float32, count = 1)
+    h['spare_end'] = np.fromfile(fid, dtype = np.float32, count = 5)
+    return h
+
+def read_data(infile, scale=True):
+    """Read any of the 4 types of image files, returns a numpy array of the image contents
+    """
+    extension = os.path.splitext(infile)[1]
+    h = read_header(infile)
+    nx = int(h['num_x_pts'])
+    ny = int(h['num_y_pts'])
+    nt = int(h['num_t_pts'])
+    fid = open(infile, 'rb')
+    fid.seek(512) #skip header
+    if extension == '.aps' or extension == '.a3daps':
+        if(h['word_type']==7): #float32
+            data = np.fromfile(fid, dtype = np.float32, count = nx * ny * nt)
+        elif(h['word_type']==4): #uint16
+            data = np.fromfile(fid, dtype = np.uint16, count = nx * ny * nt)
+        data = data.reshape(nx, ny, nt, order='F').copy() #make N-d image
+        if scale:
+            data = data * h['data_scale_factor'] #scaling factor
+        else:
+            data = (data, h['data_scale_factor'])
+    elif extension == '.a3d':
+        if(h['word_type']==7): #float32
+            data = np.fromfile(fid, dtype = np.float32, count = nx * ny * nt)
+        elif(h['word_type']==4): #uint16
+            data = np.fromfile(fid, dtype = np.uint16, count = nx * ny * nt)
+        data = data * h['data_scale_factor'] #scaling factor
+        data = data.reshape(nx, nt, ny, order='F').copy() #make N-d image
+    elif extension == '.ahi':
+        data = np.fromfile(fid, dtype = np.float32, count = 2* nx * ny * nt)
+        data = data.reshape(2, ny, nx, nt, order='F').copy()
+        real = data[0,:,:,:].copy()
+        imag = data[1,:,:,:].copy()
+    fid.close()
+    if extension != '.ahi':
+        return data
+    else:
+        return real, imag
+
+def get_x_views(filename, x=4):
+    data = read_data(filename)
+    # Upright the image
+    views = data.shape[2]
+    return [np.flipud(data[:, :, i].transpose()) for i in range(0, views, views // x)]
+
+
+class TransformDataset(torch.utils.data.Dataset):
+    # Same as a normal Dataset but randomly augments the data. 
+    # Augmentations are done asynchronously on CPU while the previous batch goes through GPU.
+    def __init__(self, data_tensor, target_tensor, names, train):
+        assert data_tensor.size(0) == target_tensor.size(0) and type(names[0]) is str
+        self.data_tensor = data_tensor
+        self.target_tensor = target_tensor
+        self.names = names
+        self.train = train
+
+    def __getitem__(self, index):
+        np.random.seed()
+        name = self.names[index]
+        data = self.data_tensor[index]
+        target = self.target_tensor[index]
+        if self.train:
+            data, target = self._transform_sample(data, target)
+        return data, target
+
+    def __len__(self):
+        return self.data_tensor.size(0)
+
+    def _transform_sample(self, im, target):
+        body_zones_flipped = dict([(1,3), (2,4), (3,1), (4,2), (5,5), (6,7), (7,6), (8,10), (9,9), (10,8), (11,12), (12,11), (13,14), (14,13), (15,16), (16,15), (17,17)])
+        def invert_target(target):
+            inverted_target = torch.Tensor(target.shape)
+            for k, v in body_zones_flipped.items():
+                inverted_target[k-1] = target[v-1]
+            return inverted_target
+
+        invert = np.random.randint(2)
+        if invert:
+            target = invert_target(target)
+        im = im.numpy()
+        if invert:
+            im = np.flip(im, 3).copy()
+            im = np.flip(im, 0).copy()
+            im = np.roll(im, 1, 0)
+        rand_int = np.random.randint(16)
+        im = np.roll(im, rand_int, 0)
+        im = torch.from_numpy(im)
+        im = im.view(-1, im.size(2), im.size(3))
+        rand_affine = torchsample.transforms.RandomAffine(
+                translation_range=[-0.01, 0.01], rotation_range=15, zoom_range=[0.95, 1.05], interp='nearest'
+        )
+        im = rand_affine(im)
+        im = im.view(16, 1, im.size(1), im.size(2))
+        return im, target
