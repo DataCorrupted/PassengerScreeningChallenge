3bb147c73b0cbf0c932c4fd4d9a84a04f1d2c866
Peter Rong <PeterRong96@gmail.com>
2018-01-04 21:05:33 +0800

    a
    Signed-off-by: Peter Rong <PeterRong96@gmail.com>

--- mvcnn.py
+++ mvcnn.py
@@ -16,7 +16,7 @@ class mvcnn(nn.Module):
         super(mvcnn, self).__init__()
 
         # Define initial pretrained CNN each view goes through
-        self.resnet = resnet50(pretrained=pretrained)
+        self.cnn = resnet50(pretrained=pretrained)
 
         # 3 convolutional layers of differing kernel size for locating threats of different size.
         self.conv1 = nn.Sequential(
@@ -57,25 +57,35 @@ class mvcnn(nn.Module):
     def forward(self, x):
         # Compute feature vector for each view
         outputs = []
-        for i in range(16):
+        for i in range(x.size()[1]):
             view = x[:, i]
-            features = self.resnet(view)    # batch x channel(2048) x 10 x 8
-            # CNN attention
-            features = torch.cat((self.conv1(features).view(features.size(0), -1), 
-                                self.conv2(features).view(features.size(0), -1),
-                                self.conv3(features).view(features.size(0), -1)), 1)
+            features = self.cnn(view)   # batch x channel(2048) x 10 x 8
+            avg_pool = self.avgpool1(features).view(features.size(0), -1)
+            attention = self.cnn_attention(avg_pool).unsqueeze(2)
+            features = torch.mul(features, attention.unsqueeze(3).expand_as(features))
+
+            # Go through each threat detection layer.
+            features = torch.cat((self.avgpool1(features).view(features.size(0), -1),
+                                    self.conv1(features).view(features.size(0), -1), 
+                                    self.conv2(features).view(features.size(0), -1),
+                                    self.conv3(features).view(features.size(0), -1)), 1)
             outputs.append(features)
 
         # Feed results to LSTM.
         outputs = torch.stack(outputs, dim=0)
+        outputs, _ = self.lstm(outputs)
 
+        attn_weights = self.softmax(
+            self.attention(outputs[-1])
+        )
         # Apply attention to the outputs and combine into a single output.
         outputs = outputs.permute(1, 0, 2)
-        print(outputs.size())
-        outputs = outputs.view((2, 16*47616))
+        attn_weights = torch.unsqueeze(attn_weights, 1)
+        outputs = torch.bmm(attn_weights, outputs)
+
         # Feed to linear classifier.
+        outputs = torch.squeeze(outputs, 1)
         outputs = self.dropout(outputs)
-        print(outputs.size())
         outputs = self.fc(outputs)
 
         return outputs
